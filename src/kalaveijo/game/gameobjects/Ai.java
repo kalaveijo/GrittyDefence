8d23fcd3eb581797b8020559a82174aae2b2cc74
kalaveijo <kalaveijo@gmail.com>
2013-11-09 15:26:30 +0200

    Drafted more AI implementation

--- src/kalaveijo/game/gameobjects/Ai.java
+++ src/kalaveijo/game/gameobjects/Ai.java
@@ -17,12 +17,23 @@ public class Ai {
 		this.targetY = currentPosY;
 		this.range = range;
 		this.u = u;
-	}
+	} // Constructor
 
 	/*
 	 * called by Unit move, checks what unit needs to do next and orders it
 	 */
 	public void assesAction() {
 
+		// Check if enemies are nearby
+		
+		// Check where to move
+		checkMovement();
+		
+	}
+	
+	private void checkMovement(){
+		// Check if next tile is free
+		
+		// Check where helper points us
 	}
 }
