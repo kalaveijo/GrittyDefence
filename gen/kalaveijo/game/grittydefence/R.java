d2074c9e4f2c440ff9773c5ca200a1d9899bd9a4
kalaveijo <kalaveijo@gmail.com>
2014-02-20 19:57:03 +0200

    added default spritesheet

--- gen/kalaveijo/game/grittydefence/R.java
+++ gen/kalaveijo/game/grittydefence/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int arrow_left=0x7f020001;
         public static final int arrow_right=0x7f020002;
         public static final int arrow_up=0x7f020003;
-        public static final int grass_field=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int spawn=0x7f020006;
+        public static final int default_spritesheet=0x7f020004;
+        public static final int grass_field=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int spawn=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
