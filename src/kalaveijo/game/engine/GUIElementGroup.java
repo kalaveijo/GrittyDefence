c7691115295d7c653eefdef62d909ac817b252c2
kalaveijo <kalaveijo@gmail.com>
2014-05-05 22:08:50 +0300

    untested crap

--- src/kalaveijo/game/engine/GUIElementGroup.java
+++ src/kalaveijo/game/engine/GUIElementGroup.java
@@ -40,4 +40,8 @@ public class GUIElementGroup {
 		return null;
 	}
 
+	public String getName() {
+		return name;
+	}
+
 }
--- src/kalaveijo/game/engine/manager/GUIManager.java
+++ src/kalaveijo/game/engine/manager/GUIManager.java
@@ -2,8 +2,12 @@ package kalaveijo.game.engine.manager;
 
 import java.util.ArrayList;
 
+import kalaveijo.game.engine.GUIElement;
 import kalaveijo.game.engine.GUIElementGroup;
+import kalaveijo.game.util.MapLocation;
+import kalaveijo.game.util.Options;
 import android.graphics.Canvas;
+import android.graphics.Point;
 
 /*
  * keeps track which gui elements are active and handles switching between elements based on input
@@ -20,6 +24,35 @@ public class GUIManager {
 		for (GUIElementGroup e : GUIElementList) {
 			e.draw(c);
 		}
+	}
+
+	// creates unit ring at maplocation
+	public void createUnitRing(MapLocation ml) {
+
+		Point center = new Point(ml.x * Options.TILE_SIZE
+				+ (Options.TILE_SIZE / 2), ml.y * Options.TILE_SIZE
+				+ (Options.TILE_SIZE / 2));
+
+		// create unitRing group
+		GUIElementGroup unitRing = new GUIElementGroup("unitRing");
+
+		// add units
+		// to the right of center
+		unitRing.addGUIElement(new GUIElement(null, "Machinegunner", new Point(
+				center.x + 40, center.y)));
+
+	}
+
+	// removes unitring
+	public void removeUnitRing() {
+		GUIElementGroup removeGroup = null;
+		for (GUIElementGroup e : GUIElementList) {
+			if (e.getName().equals("unitRing")) {
+				removeGroup = e;
+			}
+		}
+		if (removeGroup != null)
+			GUIElementList.remove(removeGroup);
 	}
 
 }
