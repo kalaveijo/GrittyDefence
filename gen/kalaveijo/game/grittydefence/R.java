7a38490053215706292eb77b8a871956d507f74c
kalaveijo <kalaveijo@gmail.com>
2015-01-20 19:53:40 +0200

    added farmer ingame

--- gen/kalaveijo/game/grittydefence/R.java
+++ gen/kalaveijo/game/grittydefence/R.java
@@ -30,15 +30,16 @@ public final class R {
         public static final int arrow_up_left=0x7f020006;
         public static final int artillerybeacon=0x7f020007;
         public static final int default_spritesheet=0x7f020008;
-        public static final int fistmecha=0x7f020009;
-        public static final int grass_field=0x7f02000a;
-        public static final int ic_launcher=0x7f02000b;
-        public static final int machinegunner=0x7f02000c;
-        public static final int mine=0x7f02000d;
-        public static final int mortar=0x7f02000e;
-        public static final int rifleman=0x7f02000f;
-        public static final int spawn=0x7f020010;
-        public static final int wall=0x7f020011;
+        public static final int farmer=0x7f020009;
+        public static final int fistmecha=0x7f02000a;
+        public static final int grass_field=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int machinegunner=0x7f02000d;
+        public static final int mine=0x7f02000e;
+        public static final int mortar=0x7f02000f;
+        public static final int rifleman=0x7f020010;
+        public static final int spawn=0x7f020011;
+        public static final int wall=0x7f020012;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
